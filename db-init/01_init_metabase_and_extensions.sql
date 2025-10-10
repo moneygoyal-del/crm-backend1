8cb749afef57512e0766c53234776a53c73c9c5d
Money Goyal <138269416+moneygoyal02@users.noreply.github.com>
2025-10-10 21:46:40 +0530

    intial

---
+++ db-init/01_init_metabase_and_extensions.sql
@@ -0,0 +1,15 @@
+-- create uuid extension for the app
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- create metabase DB and user (used by Metabase to persist its app DB)
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_user WHERE usename = 'metabase') THEN
+      CREATE USER metabase WITH PASSWORD 'MMoo@1234';
+   END IF;
+END
+$$;
+
+CREATE DATABASE metabase OWNER metabase;
+GRANT ALL PRIVILEGES ON DATABASE metabase TO metabase;
