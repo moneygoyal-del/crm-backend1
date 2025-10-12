fbdcee53633ac6bbf1a5ad9eacd0e3110b94b583
ankit <closetooankit@gmail.com>
2025-10-12 14:53:05 +0530

    doctor routes

---
+++ backend/src/routes/doctor.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+// router.route("create").post();
+// router.route("delete").delete();
+// router.route("update").put();
+
+export default router;
