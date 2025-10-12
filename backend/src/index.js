b125754226a3f09baa7b628cb6054d812e5d62a3
ankit <closetooankit@gmail.com>
2025-10-12 14:51:43 +0530

    added routers

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,18 @@
 import app from "./app.js";
-import {connectDB,pool} from "./DB/db.js"
+import {connectDB} from "./DB/db.js"
 import 'dotenv/config'
+
+//routers
+import userRouter from "./routes/user.routes.js"
+import doctorRouter from "./routes/doctor.routes.js"
 
 const port = process.env.PORT || 8000;
 
 connectDB()
     .then(() => {
+        app.use("/api/v1/users",userRouter);
+        app.use("/api/v1/doctors",doctorRouter);
+
         app.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}`);
         });
