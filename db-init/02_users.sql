266e66acffe63465fe4022aea95ecfb4378199ca
Money Goyal <138269416+moneygoyal02@users.noreply.github.com>
2025-10-13 20:37:21 +0530

    add init

---
+++ db-init/02_users.sql
@@ -0,0 +1,27 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255),
+    password_hash VARCHAR(255) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100),
+    phone VARCHAR(20) UNIQUE NOT NULL,
+    role VARCHAR(50) NOT NULL DEFAULT 'agent',
+    team_id UUID, 
+    is_active BOOLEAN DEFAULT true,
+    last_login TIMESTAMP,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE teams (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL,
+    team_lead_id UUID REFERENCES users(id),
+    territory JSONB,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+ALTER TABLE users
+ADD CONSTRAINT fk_users_team_id
+FOREIGN KEY (team_id) REFERENCES teams(id);
---
+++ db-init/03_doctors.sql
@@ -0,0 +1,32 @@
+CREATE TABLE doctors (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) ,
+    phone VARCHAR(20) UNIQUE NOT NULL,
+    location JSONB, -- GPS coordinates and address
+    gps_location_link TEXT, -- GPS location link for the doctor
+    contact_preferences JSONB,
+    onboarding_date DATE,
+    status VARCHAR(20) DEFAULT 'active', -- 'active', 'inactive', 'churned'
+    last_meeting TIMESTAMP,
+    assigned_agent_id_offline UUID REFERENCES users(id),
+    assigned_agent_id_online UUID REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE doctor_meetings (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    doctor_id UUID REFERENCES doctors(id),
+    agent_id UUID REFERENCES users(id),
+    meeting_type VARCHAR(20) NOT NULL, -- 'physical', 'virtual', 'call'
+    duration INTEGER, -- Duration in minutes
+    location JSONB, -- GPS coordinates, address
+    gps_location_link TEXT,
+    meeting_notes TEXT,
+    photos JSONB, -- Array of photo URLs
+    gps_verified BOOLEAN DEFAULT false,
+    meeting_summary VARCHAR(50),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
