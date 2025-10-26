adeab145d9daa7092b88e9c59efd89dd7acbf362
Money Goyal <138269416+moneygoyal02@users.noreply.github.com>
2025-10-26 18:38:29 +0530

    feat(docker-compose): Change backend service port to 9001 and add health check endpoint in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,19 @@ connectDB()
         app.use("/api/v1/users",userRouter);
         app.use("/api/v1/doctors",doctorRouter);
         app.use("/api/v1/patientLeads",patientLeadRouter);
+        app.get("/", (req, res) => {
+            res.send("Welcome to the Medpho CRM Backend API");
+        });
+        
+        
+        app.get("/api/v1/health", (req, res) => {
+            res.status(200).json({
+                message: "Server is Up and Running!",
+                service: "medpho-crm-backend",
+                timestamp: new Date().toISOString()
+            });
+        });
+        // ---------------------------------
 
         app.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}`);
